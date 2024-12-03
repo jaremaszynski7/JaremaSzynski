c7bcb1bd3d9a9962199dc9b13ab18338d62e3ae0
Jarema Szynski <jaremaszynski7@mat.umk.pl>
2024-12-03 23:29:04 +0100

    sprawdzanie czy liczby sa dodatnie

--- podzielnosc.cpp
+++ podzielnosc.cpp
@@ -5,6 +5,10 @@ using namespace std;
 int main(){
 	int n,m;
 	cin >> n >> m;
+	if(n <= 0 || m <= 0){
+		cout << "Wczytane liczby musza byc dodatnie" << endl;
+		return 0;
+	}
 	cout << n << " " << m << endl;
 	return 0;
 }
