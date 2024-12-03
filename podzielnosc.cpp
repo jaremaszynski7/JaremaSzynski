478c61fc111d440c3439d70186d6c4d58b1d3fec
Jarema Szynski <jaremaszynski7@mat.umk.pl>
2024-12-03 23:22:17 +0100

    druga migawka

--- podzielnosc.cpp
+++ podzielnosc.cpp
@@ -2,9 +2,20 @@
 
 using namespace std;
 
+int nwd(int n, int m){
+	while(n!=m){
+		if(n>m){
+			n-=m;
+		}else{
+			m-=n;
+		}
+	}
+	return n;
+}
+
 int main(){
 	int n,m;
 	cin >> n >> m;
-	cout << n << " " << m << endl;
+	cout << n << " " << m << " NWD tych liczb to: " << nwd(n,m) <<  endl;
 	return 0;
 }
